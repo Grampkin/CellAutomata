88109dbdf4933aca9a2dee446a7e55c5968fb6e0
Grampkin <khoruzhenkomisha@gmail.com>
2018-12-06 03:47:13 +0100

    Zone detection + removing small zones

--- CelAutoPocGenTest/Assets/GenerujMape.cs
+++ CelAutoPocGenTest/Assets/GenerujMape.cs
@@ -37,19 +37,16 @@ public class GenerujMape : MonoBehaviour
 		plytka = new int[szerokosc, wysokosc];
 	    plytka2 = new int[szerokosc, wysokosc];
 
-	    
-
-	    
-
-
-
 	    WypelnijMape();
 
 		for (int i = 0; i < stopienWygl; i++) {
 			Wygladzenie ();
 		}
 
-	    int[,] mapaOgraniczona = new int[szerokosc + rozmiarGranicy * 2, wysokosc + rozmiarGranicy * 2];
+	    UsunDrobneElementy();
+
+
+        int[,] mapaOgraniczona = new int[szerokosc + rozmiarGranicy * 2, wysokosc + rozmiarGranicy * 2];
 
 	    for (int x = 0; x < mapaOgraniczona.GetLength(0); x++)
 	    {
@@ -71,8 +68,103 @@ public class GenerujMape : MonoBehaviour
 	
 	}
 
-	/* Делает стенки для карты, заполняет каждую клетку случайным цветом (черный или белый) в зависимости от процента заполнения
-	 */
+    void UsunDrobneElementy()
+    {
+        List<List<Wspolrzedne>> strefyScian = Strefa(1);
+        int rozmiarMinimalny = 20;
+
+        foreach (List<Wspolrzedne> strefaScian in strefyScian)
+        {
+            if (strefaScian.Count < rozmiarMinimalny)
+            {
+                foreach (Wspolrzedne wybranaPlytka in strefaScian)
+                {
+                    plytka[wybranaPlytka.plytkaX, wybranaPlytka.plytkaY] = 0;
+                }
+            }
+        }
+
+        List<List<Wspolrzedne>> strefyPuste = Strefa(0);
+        int pustyMinimalny = 20;
+
+        foreach (List<Wspolrzedne> strefaPuste in strefyPuste)
+        {
+            if (strefaPuste.Count < pustyMinimalny)
+            {
+                foreach (Wspolrzedne wybranaPlytka in strefaPuste)
+                {
+                    plytka[wybranaPlytka.plytkaX, wybranaPlytka.plytkaY] = 1;
+                }
+            }
+        }
+    }
+
+    List<List<Wspolrzedne>> Strefa(int rodzaj)
+    {
+        List<List<Wspolrzedne>> strefy = new List<List<Wspolrzedne>>();
+        int[,] flagi = new int[szerokosc, wysokosc];
+
+        for (int x = 0; x < szerokosc; x++)
+        {
+            for (int y = 0; y < wysokosc; y++)
+            {
+                if (flagi[x, y] == 0 && plytka[x,y] == rodzaj)
+                {
+                    List<Wspolrzedne> strefaNowa = PlytkaStrefy(x, y);
+                    strefy.Add(strefaNowa);
+
+                    foreach (Wspolrzedne wybranaPlytka in strefaNowa)
+                    {
+                        flagi[wybranaPlytka.plytkaX, wybranaPlytka.plytkaY] = 1;
+                    }
+                }
+            }
+        }
+
+        return strefy; 
+    }
+
+    List<Wspolrzedne> PlytkaStrefy(int poczX, int poczY)
+    {
+        List<Wspolrzedne> wspolrzedne = new List<Wspolrzedne>();
+        int[,] flagi = new int[szerokosc, wysokosc];
+        int rodzaj = plytka[poczX, poczY];
+
+        Queue<Wspolrzedne> queue = new Queue<Wspolrzedne>();
+        queue.Enqueue(new Wspolrzedne(poczX, poczY));
+        flagi[poczX, poczY] = 1;
+
+        while (queue.Count > 0)
+        {
+            Wspolrzedne wybranaPlytka = queue.Dequeue();
+            wspolrzedne.Add(wybranaPlytka);
+
+            for (int x = wybranaPlytka.plytkaX - 1; x <= wybranaPlytka.plytkaX + 1; x++)
+            {
+                for (int y = wybranaPlytka.plytkaY - 1; y <= wybranaPlytka.plytkaY + 1; y++)
+                {
+                    if ((x >= 0 && x < szerokosc && y >= 0 && y < wysokosc) &&
+                        (x == wybranaPlytka.plytkaX || y == wybranaPlytka.plytkaY))
+                    {
+                        if (flagi[x, y] == 0 && plytka[x, y] == rodzaj)
+                        {
+                            flagi[x, y] = 1;
+                            queue.Enqueue(new Wspolrzedne(x, y));
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return wspolrzedne;
+    }
+
+
+
+
+    /* Делает стенки для карты, заполняет каждую клетку случайным цветом (черный или белый) в зависимости от процента заполнения
+     */
 
 	void WypelnijMape() {
 		if (seedCheck == true) {
@@ -95,6 +187,18 @@ public class GenerujMape : MonoBehaviour
 			}
 		}
 	}
+
+    public struct Wspolrzedne
+    {
+        public int plytkaX;
+        public int plytkaY;
+
+        public Wspolrzedne(int x, int y)
+        {
+            plytkaX = x;
+            plytkaY = y; 
+        }
+    }
 
 	void Wygladzenie () {
 
