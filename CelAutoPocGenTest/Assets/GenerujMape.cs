11dd59c444fea41656889e6fa926558850810381
Grampkin <khoruzhenkomisha@gmail.com>
2018-12-07 22:19:16 +0100

    Rooms connection

--- CelAutoPocGenTest/Assets/GenerujMape.cs
+++ CelAutoPocGenTest/Assets/GenerujMape.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 public class GenerujMape : MonoBehaviour
 {
@@ -16,8 +17,8 @@ public class GenerujMape : MonoBehaviour
     
 
 
-	int [,] plytka;
-    private int[,] plytka2;
+	int [,] poziom;
+    private int[,] poziom2;
 
 	[Range(0,100)]
 	public int udzialNapelnienia;
@@ -34,8 +35,8 @@ public class GenerujMape : MonoBehaviour
 
 	void Generuj () {
 	
-		plytka = new int[szerokosc, wysokosc];
-	    plytka2 = new int[szerokosc, wysokosc];
+		poziom = new int[szerokosc, wysokosc];
+	    poziom2 = new int[szerokosc, wysokosc];
 
 	    WypelnijMape();
 
@@ -54,7 +55,7 @@ public class GenerujMape : MonoBehaviour
 	        {
 	            if (x >= rozmiarGranicy && x < szerokosc + rozmiarGranicy && y >= rozmiarGranicy && y < wysokosc + rozmiarGranicy)
 	            {
-	                mapaOgraniczona[x, y] = plytka[x - rozmiarGranicy, y - rozmiarGranicy];
+	                mapaOgraniczona[x, y] = poziom[x - rozmiarGranicy, y - rozmiarGranicy];
 	            }
 	            else
 	            {
@@ -71,7 +72,10 @@ public class GenerujMape : MonoBehaviour
     void UsunDrobneElementy()
     {
         List<List<Wspolrzedne>> strefyScian = Strefa(1);
-        int rozmiarMinimalny = 20;
+        int rozmiarMinimalny = 40;
+        List<Pokoj> pokojePoFiltrowaniu = new List<Pokoj>();
+
+
 
         foreach (List<Wspolrzedne> strefaScian in strefyScian)
         {
@@ -79,24 +83,31 @@ public class GenerujMape : MonoBehaviour
             {
                 foreach (Wspolrzedne wybranaPlytka in strefaScian)
                 {
-                    plytka[wybranaPlytka.plytkaX, wybranaPlytka.plytkaY] = 0;
+                    poziom[wybranaPlytka.plytkaX, wybranaPlytka.plytkaY] = 0;
                 }
             }
         }
 
         List<List<Wspolrzedne>> strefyPuste = Strefa(0);
-        int pustyMinimalny = 20;
+        int pustyMinimalny = 40;
 
-        foreach (List<Wspolrzedne> strefaPuste in strefyPuste)
+        foreach (List<Wspolrzedne> strefaPusta in strefyPuste)
         {
-            if (strefaPuste.Count < pustyMinimalny)
+            if (strefaPusta.Count < pustyMinimalny)
             {
-                foreach (Wspolrzedne wybranaPlytka in strefaPuste)
+                foreach (Wspolrzedne wybranaPlytka in strefaPusta)
                 {
-                    plytka[wybranaPlytka.plytkaX, wybranaPlytka.plytkaY] = 1;
+                    poziom[wybranaPlytka.plytkaX, wybranaPlytka.plytkaY] = 1;
                 }
+
+            }
+            else
+            {
+                pokojePoFiltrowaniu.Add(new Pokoj(strefaPusta, poziom));
             }
         }
+
+        PolaczNajblizszePokoje(pokojePoFiltrowaniu);
     }
 
     List<List<Wspolrzedne>> Strefa(int rodzaj)
@@ -108,7 +119,7 @@ public class GenerujMape : MonoBehaviour
         {
             for (int y = 0; y < wysokosc; y++)
             {
-                if (flagi[x, y] == 0 && plytka[x,y] == rodzaj)
+                if (flagi[x, y] == 0 && poziom[x,y] == rodzaj)
                 {
                     List<Wspolrzedne> strefaNowa = PlytkaStrefy(x, y);
                     strefy.Add(strefaNowa);
@@ -128,7 +139,7 @@ public class GenerujMape : MonoBehaviour
     {
         List<Wspolrzedne> wspolrzedne = new List<Wspolrzedne>();
         int[,] flagi = new int[szerokosc, wysokosc];
-        int rodzaj = plytka[poczX, poczY];
+        int rodzaj = poziom[poczX, poczY];
 
         Queue<Wspolrzedne> queue = new Queue<Wspolrzedne>();
         queue.Enqueue(new Wspolrzedne(poczX, poczY));
@@ -146,7 +157,7 @@ public class GenerujMape : MonoBehaviour
                     if ((x >= 0 && x < szerokosc && y >= 0 && y < wysokosc) &&
                         (x == wybranaPlytka.plytkaX || y == wybranaPlytka.plytkaY))
                     {
-                        if (flagi[x, y] == 0 && plytka[x, y] == rodzaj)
+                        if (flagi[x, y] == 0 && poziom[x, y] == rodzaj)
                         {
                             flagi[x, y] = 1;
                             queue.Enqueue(new Wspolrzedne(x, y));
@@ -176,17 +187,99 @@ public class GenerujMape : MonoBehaviour
 		for (int x = 0; x < szerokosc; x++) {
 			for (int y = 0; y < wysokosc; y++) {
 				if (x == 0 || x == szerokosc-1 || y == 0 || y == wysokosc - 1)
-					plytka [x, y] = plytka2[x, y] = 1;
+					poziom [x, y] = poziom2[x, y] = 1;
 				else {
 					if (generatorLiczby.Next (0, 100) < udzialNapelnienia)
-						plytka [x, y] = plytka2[x, y] = 1;
+						poziom [x, y] = poziom2[x, y] = 1;
 					else
-						plytka [x, y] = plytka2[x, y] = 0;
+						poziom [x, y] = poziom2[x, y] = 0;
 				}
 
 			}
 		}
 	}
+
+    void PolaczNajblizszePokoje(List<Pokoj> pokoje)
+    {
+        int odlegloscMin = 0;
+
+        Wspolrzedne najblizszaPlytka1 = new Wspolrzedne();
+        Wspolrzedne najblizszaPlytka2 = new Wspolrzedne();
+
+        Pokoj najblizszyPokoj1 = new Pokoj();
+        Pokoj najblizszyPokoj2 = new Pokoj();
+
+        bool polaczenieMozliwe = false;
+
+
+
+        foreach (Pokoj pokoj1 in pokoje)
+        {
+            polaczenieMozliwe = false;
+
+            foreach (Pokoj pokoj2 in pokoje)
+            {
+                if (pokoj1 == pokoj2)
+                {
+                    continue;
+                }
+
+                if (pokoj1.czyPolaczone(pokoj2))
+                {
+                    polaczenieMozliwe = false;
+                    break;
+                }
+
+                for (int granicznaPlytka1 = 0; granicznaPlytka1 < pokoj1.plytkiGraniczne.Count; granicznaPlytka1++)
+                {
+                    for (int granicznaPlytka2 = 0; granicznaPlytka2 < pokoj2.plytkiGraniczne.Count; granicznaPlytka2++)
+                    {
+
+                        Wspolrzedne plytka1 = pokoj1.plytkiGraniczne[granicznaPlytka1];
+                        Wspolrzedne plytka2 = pokoj2.plytkiGraniczne[granicznaPlytka2];
+
+                        int odleglosc =
+                            (int) (((plytka1.plytkaX - plytka2.plytkaX) * (plytka1.plytkaX - plytka2.plytkaX)) +
+                                   ((plytka1.plytkaY - plytka2.plytkaY) * (plytka1.plytkaY - plytka2.plytkaY)));
+
+                        if (odleglosc < odlegloscMin || !polaczenieMozliwe)
+                        {
+                            odlegloscMin = odleglosc;
+                            polaczenieMozliwe = true;
+
+                            najblizszaPlytka1 = plytka1;
+                            najblizszaPlytka2 = plytka2;
+
+                            najblizszyPokoj1 = pokoj1;
+                            najblizszyPokoj2 = pokoj2;
+
+                        }
+
+
+                    }
+                }
+            }
+        }
+        if (polaczenieMozliwe)
+        {
+            UtworzKorytaz(najblizszyPokoj1, najblizszyPokoj2, najblizszaPlytka1, najblizszaPlytka2);
+
+        }
+    }
+
+    void UtworzKorytaz(Pokoj pokoj1, Pokoj pokoj2, Wspolrzedne plytka1, Wspolrzedne plytka2)
+    {
+        Pokoj.PolaczPokoje(pokoj1, pokoj2);
+            
+        Debug.DrawLine(WspolrzedneToVector3(plytka1),WspolrzedneToVector3(plytka2), Color.cyan, 10);
+    }
+
+    
+
+    Vector3 WspolrzedneToVector3(Wspolrzedne plytka)
+    {
+        return new Vector3(-szerokosc/2 + .5f + plytka.plytkaX, 2, -wysokosc/2 + .5f + plytka.plytkaY);
+    }
 
     public struct Wspolrzedne
     {
@@ -200,18 +293,68 @@ public class GenerujMape : MonoBehaviour
         }
     }
 
-	void Wygladzenie () {
+    public class Pokoj
+    {
+        public List<Wspolrzedne> wspolrzednePlytki;
+        public List<Wspolrzedne> plytkiGraniczne;
+        public List<Pokoj> pokojePolaczone;
+        public int rozmiarPokoju;
+
+        public Pokoj() { }
+
+
+        public Pokoj(List<Wspolrzedne> plytkiPokoju, int[,] poziom)
+        {
+            wspolrzednePlytki = plytkiPokoju;
+            rozmiarPokoju = wspolrzednePlytki.Count;
+
+            pokojePolaczone = new List<Pokoj>();
+            plytkiGraniczne = new List<Wspolrzedne>();
+
+            foreach (Wspolrzedne plytka in wspolrzednePlytki)
+            {
+                for (int x = plytka.plytkaX - 1; x <= plytka.plytkaX + 1; x++)
+                {
+                    for (int y = plytka.plytkaY - 1; y <= plytka.plytkaY + 1; y++)
+                    {
+                        if (x == plytka.plytkaX || y == plytka.plytkaY)
+                        {
+                            if (poziom[x,y] == 1)
+                            {
+                                plytkiGraniczne.Add(plytka);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        public static void PolaczPokoje(Pokoj pokoj1, Pokoj pokoj2)
+        {
+            pokoj1.pokojePolaczone.Add(pokoj2);
+            pokoj2.pokojePolaczone.Add(pokoj1);
+        }
+
+        public bool czyPolaczone(Pokoj pokojDowolny)
+        {
+            return pokojePolaczone.Contains(pokojDowolny);
+        }
+    }
+
+
+    void Wygladzenie () {
 
 
         for (int x = 0; x < szerokosc; x++) {
 			for (int y = 0; y < wysokosc; y++)
 			{
-			    plytka[x, y] = plytka2[x, y]; //чисто на погчампе записываешь состояние клетки параллельно 
+			    poziom[x, y] = poziom2[x, y]; //чисто на погчампе записываешь состояние клетки параллельно 
 				int przyleglaPlytka = LiczbaScian (x, y);
 				if (przyleglaPlytka > 4)
-					plytka2 [x, y] = 1;
+					poziom2 [x, y] = 1;
 				else if (przyleglaPlytka < 4)
-					plytka2 [x, y] = 0;
+					poziom2 [x, y] = 0;
 			}
 		}
 		
@@ -227,7 +370,7 @@ public class GenerujMape : MonoBehaviour
 			    //если соседняя клетка находится в пределах карты то берется её состояние (1 либо 0)
                 if (ssX >= 0 && ssX < szerokosc && ssY >= 0 && ssY < wysokosc) {    
 					if (ssX != scX || ssY != scY) {
-						licznik += plytka [ssX, ssY];
+						licznik += poziom [ssX, ssY];
                     }
 				}
                 //если соседняя клетка находится вне карты то за её состояние принимается единица 
@@ -244,23 +387,26 @@ public class GenerujMape : MonoBehaviour
 	}
 
 
-	void OnDrawGizmos() {
-		/*
-		if (plytka != null) {
-			for (int x = 0; x < szerokosc; x++) {
-				for (int y = 0; y < wysokosc; y++) {
-					if (plytka [x, y] == 1)
-						Gizmos.color = Color.black;
-					else
-						Gizmos.color = Color.white;
-					Vector3 poz = new Vector3 (-szerokosc/2 + x + .5f, 0.0f, -wysokosc/2 + y + .5f);
-					Gizmos.DrawCube (poz, new Vector3(1,0,1));
 
-				}
-			}
-		}
-		*/
-	}
+	//void OnDrawGizmos() {
+		
+	    
+	//    /*
+	//	if (poziom != null) {
+	//		for (int x = 0; x < szerokosc; x++) {
+	//			for (int y = 0; y < wysokosc; y++) {
+	//				if (poziom [x, y] == 1)
+	//					Gizmos.color = Color.black;
+	//				else
+	//					Gizmos.color = Color.white;
+	//				Vector3 poz = new Vector3 (-szerokosc/2 + x + .5f, 0.0f, -wysokosc/2 + y + .5f);
+	//				Gizmos.DrawCube (poz, new Vector3(1,0,1));
+
+	//			}
+	//		}
+	//	}
+	//	*/
+	//}
 
 
 }
